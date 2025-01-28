4e3ca8b473397e2c27e7f0b062a1b420352ba448
Kisarayny <101909686+Kisarayny@users.noreply.github.com>
2025-01-27 19:32:19 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hoia_mundo_sarai',
+  webDir: 'www'
+};
+
+export default config;
